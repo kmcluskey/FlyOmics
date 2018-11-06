f4d5f27af9b32b335e9a44a6b94570d1f3149eec
Karen McLuskey <Karen.McLuskey@me.com>
2018-11-06 14:30:54 +0000

    Initial setup

---
+++ met_explore/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the met_expore index page.")
+
+# Create your views here.
